10e891381fef8baab9f723fdd1f28ebba7cae924
Express-Mail-13 <expressmail.provider@gmail.com>
2025-01-12 15:47:10 +0530

    Added command to genapp.py at line 10

--- genapp.py
+++ genapp.py
@@ -7,6 +7,7 @@ print('Command for file1')
 print('Command for file1')
 apikey="#"
 
+print('Command for file2')
 genai.configure(api_key=apikey)
 print('Command for file2')
 model = genai.GenerativeModel('gemini-pro')
